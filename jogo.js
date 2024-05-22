75117c00404088a3673247184834b72197134509
carlosrenatoalves <carlosrenatoalvesdeoliveira@gmail.com>
2024-05-22 11:07:36 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -1,15 +1,19 @@
 
 const quadrado = document.getElementById("quadrado");
 const comida = document.getElementById("comida");
+const titulo=document.getElementById("titulo");
+var largura=10;
+var altura=10;
+var pont=0;
 
 
-function quadradoMove(x, y){
+function quadradoMove(x, y){  //faz o quadrado se mover
     quadrado.style.left=`${x}px`;
     quadrado.style.top=`${y}px`;
 
 }
 
-function calculaDistancia(x,y,x2,y2){
+function calculaDistancia(x,y,x2,y2){ // calcula a distancia entre o quadrado e a comida
     var dist;
     dist=Math.floor(Math.sqrt((x-x2)**2+(y-y2)**2));
 
@@ -17,24 +21,23 @@ return dist;
 }
 
 var bo=true;
+if(bo==true){
+    comida.style.left = `10px`;
+    comida.style.top = `10px`;
+    var posX=10;
+    var posY=10;
+    bo=false;
+}
+
+titulo.textContent=`Pontuação:${pont}`;
 
 document.addEventListener('mousemove',(event)=>{
-
-    if(bo==true){
-        comida.style.left = `10px`;
-        comida.style.top = `10px`;
-        var posX=10;
-        var posY=10;
-        bo=false;
-    }
-
-
+    
     const mousex=event.clientX;
     const mousey=event.clientY;
 
     quadradoMove(mousex,mousey);
     var dist=calculaDistancia(mousex,mousey,posX,posY);
-
     if(dist<20){
         const maxWidth = window.innerWidth - 3;
         const maxHeight = window.innerHeight - 3;
@@ -42,8 +45,12 @@ document.addEventListener('mousemove',(event)=>{
         posY = Math.floor(Math.random() * maxHeight);
         comida.style.left = `${posX}px`;
         comida.style.top = `${posY}px`;
+        largura=largura+2;
+        altura=altura+2;
+        quadrado.style.width=`${largura}px`;
+        quadrado.style.height=`${altura}px`;
+        pont++;
+        titulo.textContent=`Pontuação:${pont}`;
     }
-
-
 });
 
